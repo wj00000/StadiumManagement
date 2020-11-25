f0d77b4cb4ea2217c4cb7e80ac4853b1348e0b24
wj <675393913@qq.com>
2020-11-25 17:49:34 +0800

    初始化

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+Route::get('/', 'WebController@init');
 Route::get('/wx-init','WebController@init');
