1026e6295e0bbe3b0c202d8231cfc0ddecb00c69
wj <675393913@qq.com>
2020-11-28 10:28:36 +0800

    初始化

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use Laravel\Passport\Passport;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -24,7 +26,11 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
+        Passport::routes();
 
+        Passport::tokensExpireIn(Carbon::now()->addDays(1));
+
+        Passport::refreshTokensExpireIn(Carbon::now()->addDays(2));
         //
     }
 }
